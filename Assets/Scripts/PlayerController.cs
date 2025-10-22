ad89dda6808324b4625e2190c1f55c013dc45c68
Elixa-4 <4emmac@gmail.com>
2025-10-21 23:09:25 -0700

    Done with player movement and rotation and camera rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,81 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    //Movement vars
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gravity = -9.81f;
     private CharacterController controller;
-    private Vector3 moveInput;
+    private Vector2 moveInput;
     private Vector3 velocity;
+    
+    //Camera vars
+    [Header("Camera")]
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float lookSensitivity = 0.5f;
+    [SerializeField] private float maxLookAngle = 80f;
+    private Vector2 rotationInput;
+    private float cameraPitch = 0f;
 
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+
+        //Lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+        HandleMovement();
+        HandleRotation();
+        ApplyGravity();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
         controller.Move(move * moveSpeed * Time.deltaTime);
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void HandleRotation()
     {
-        moveInput = context.ReadValue<Vector2>();
+        // Horizontal rotation (left/right) - rotate player body
+        transform.Rotate(Vector3.up, rotationInput.x * lookSensitivity);
+
+        // Vertical rotation (up/down) - rotate camera only
+        cameraPitch -= rotationInput.y * lookSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -maxLookAngle, maxLookAngle);
+        playerCamera.localEulerAngles = new Vector3(cameraPitch, 0f, 0f);
+    }
+
+    private void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    //Called when WASD keys pressed
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    //Called when mouse position moves
+    public void OnRotation(InputValue value)
+    {
+        rotationInput = value.Get<Vector2>();
     }
 }
