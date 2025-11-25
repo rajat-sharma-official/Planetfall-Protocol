87cca8187bf8625ef6d494ebe323e9167d6a1d54
bpicassokennedy <b3llakennedy@gmail.com>
2025-11-24 19:19:23 -0800

    more changes
    more changes to vera menu

---
+++ Assets/Scripts/VERAMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class VERAMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
