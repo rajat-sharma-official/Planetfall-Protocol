37b32d9b9ece90a3a280c837ad6bb5c75cb99877
Elixa-4 <4emmac@gmail.com>
2025-12-04 17:52:14 -0800

    Added Harvel NPC and conversation, disabled movement during conversation

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,14 +1,22 @@
 using TMPro;
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class DialogueManager : MonoBehaviour
 {
     [Header("Dialogue UI")]
-    [SerializeField] private GameObject dialoguePanel;      // assign Dialogue Panel
-    [SerializeField] private TextMeshProUGUI dialogueText;  // assign DialogueText
+    [SerializeField] private GameObject dialoguePanel; 
+    [SerializeField] private TextMeshProUGUI dialogueText; 
+    [SerializeField] private GameObject choicePanel;
+    [SerializeField] private TextMeshProUGUI choice1Text;
+    [SerializeField] private TextMeshProUGUI choice2Text;
+    [SerializeField] private TextMeshProUGUI choice3Text;
+    [SerializeField] private TextMeshProUGUI choice4Text;
   
     private static DialogueManager instance;
+
+    public static event Action<int> OnChoiceClicked;
 
     private void Awake()
     {
@@ -30,6 +38,7 @@ public class DialogueManager : MonoBehaviour
     private void Start()
     {
         HideDialogue();
+        HideChoices();
     }
 
     //Hide the panel and clear any text.
@@ -55,8 +64,6 @@ public class DialogueManager : MonoBehaviour
     //Show the panel and append a new line of text.
     public void AppendLine(string text)
     {
-        Debug.Log($"[DialogueManager] AppendLine called with: {text}");
-
         if (dialoguePanel != null)
             dialoguePanel.SetActive(true);
 
@@ -71,4 +78,47 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text += text;
     }
 
+    public void HideChoices()
+    {
+        if (choicePanel != null)
+            choicePanel.SetActive(false);
+        
+    }
+
+    public void ShowChoices(string choice1, string choice2, string choice3, string choice4)
+    {
+        Debug.Log($"ShowChoices called - choice1: '{choice1}', choice2: '{choice2}', choice3: '{choice3}', choice4: '{choice4}'");
+
+        if(choicePanel != null)
+            choicePanel.SetActive(true);
+
+        if(choice1Text != null)
+            choice1Text.text = choice1;
+        if(choice2Text != null)
+            choice2Text.text = choice2;
+        if(choice3Text != null)
+            choice3Text.text = choice3;
+        if(choice4Text != null)
+            choice4Text.text = choice4;
+    }
+
+    public void Choice1Clicked()
+    {
+        OnChoiceClicked?.Invoke(1);
+    }
+
+    public void Choice2Clicked()
+    {
+        OnChoiceClicked?.Invoke(2);
+    }
+
+    public void Choice3Clicked()
+    {
+        OnChoiceClicked?.Invoke(3);
+    }
+
+    public void Choice4Clicked()
+    {
+        OnChoiceClicked?.Invoke(4);
+    }
 }
