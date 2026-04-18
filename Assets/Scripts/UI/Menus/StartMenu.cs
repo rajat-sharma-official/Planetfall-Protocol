ed56c8954d9d5323680cf147a3bdea07716d2a3d
Elixa-4 <4emmac@gmail.com>
2026-04-18 13:27:14 -0700

    Updated EndGame menu to match StartMenu

--- Assets/Scripts/UI/Menus/StartMenu.cs
+++ Assets/Scripts/UI/Menus/StartMenu.cs
@@ -5,20 +5,30 @@ using UnityEngine.UI;
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] private Button loadGameButton;
+    private AudioManager audioManager;
+
+
+    private void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
 
     private void Start()
     {
+        audioManager.Play("Start");
         loadGameButton.interactable = DataPersistenceManager.instance.HasSaveData();
     }
 
     public void OnNewGameClicked()
     {
+        audioManager.Stop("Start");
         DataPersistenceManager.instance.NewGame();
         SceneManager.LoadScene("BaseScene");
     }
 
     public void OnLoadGameClicked()
     {
+        audioManager.Stop("Start");
         DataPersistenceManager.instance.LoadGame();
         SceneManager.LoadScene("BaseScene");
     }
